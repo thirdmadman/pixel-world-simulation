94356a153e89bafe4b6317d634b9473f6cec3955
thirdmadman <thirdmadman@gmail.com>
2024-03-07 13:53:34 +0100

    feat: add layer

--- src/app/render/Renderer.ts
+++ src/app/render/Renderer.ts
@@ -1,5 +1,13 @@
 import { mixColors } from '../utils/utils';
 
+interface IPixelsLayer {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+  pixels: Uint32Array;
+}
+
 export class Renderer {
   private canvas: HTMLCanvasElement;
 
@@ -68,36 +76,69 @@ export class Renderer {
 
   getLastFrameTime = () => this.lastFrameTime;
 
-  blendPixelLayers(layers: Array<Array<Array<number>>>) {
+  // eslint-disable-next-line class-methods-use-this
+  blendPixelLayers(layers: Array<IPixelsLayer>, width: number, height: number) {
+    const toLinearArrayIndex = (x: number, y: number, w: number, h: number) => (h - y - 1) * w + x;
+
     if (!layers || layers.length === 0) return null;
 
-    if (!layers.every((el) => el.length === this.width)) {
-      return null;
-    }
-
     if (layers.length === 1) {
-      return layers[0];
+      return layers[0].pixels;
     }
 
-    const resultOfBlend: Array<Array<number | null>> = [];
+    const resultOfBlend = new Uint32Array(width * height);
 
-    for (let row = 0; row < this.height / this.pixelSize; row++) {
-      const blendedPixelsRow: Array<number | null> = [];
-      for (let col = 0; col < this.width / this.pixelSize; col++) {
-        let tmpColorBlend: null | number = null;
-        for (let layerIndex = 0; layerIndex < layers.length; layerIndex += 1) {
-          if (layers[layerIndex][row][col] !== undefined && layers[layerIndex][row][col] !== null) {
-            if (layerIndex === 0) {
-              tmpColorBlend = layers[layerIndex][row][col];
-            } else if (tmpColorBlend !== null) {
-              tmpColorBlend = Number(mixColors(tmpColorBlend, layers[layerIndex][row][col]));
-            }
-            blendedPixelsRow[col] = tmpColorBlend;
+    for (let layerIndex = layers.length - 1; layerIndex >= 0; layerIndex--) {
+      const layer = layers[layerIndex];
+
+      // if (layerIndex === layers.length - 1) {
+      //   console.error(layerIndex);
+      // }
+
+      const layerWidth = layer.width;
+      const layerHeight = layer.height;
+      const layerX = layer.x;
+      const layerY = layer.y;
+
+      for (let layerRow = 0; layerRow < layerHeight; layerRow++) {
+        const cyclicCol = (layerHeight - 1 - layerRow) * layerWidth;
+        for (let layerCol = cyclicCol; layerCol < cyclicCol + layerWidth; layerCol++) {
+          const normalizedCol = layerCol - cyclicCol;
+
+          const pixelPositionX = layerX + normalizedCol;
+          const pixelPositionY = layerY + layerRow;
+
+          const realPositionOfPixel = toLinearArrayIndex(pixelPositionX, pixelPositionY, width, height);
+
+          const blendResultPixel = resultOfBlend[realPositionOfPixel];
+          const layerPixel = layer.pixels[layerCol];
+
+          if (blendResultPixel === null || blendResultPixel === undefined || blendResultPixel === 0) {
+            resultOfBlend[realPositionOfPixel] = layerPixel;
+          } else if (layerPixel !== null && layerPixel !== undefined) {
+            resultOfBlend[realPositionOfPixel] = Number(mixColors(blendResultPixel, layerPixel));
           }
-          resultOfBlend[row] = blendedPixelsRow;
         }
       }
     }
+
+    // for (let row = 0; row < this.height / this.pixelSize; row++) {
+    //   const blendedPixelsRow: Array<number | null> = [];
+    //   for (let col = 0; col < this.width / this.pixelSize; col++) {
+    //     let tmpColorBlend: null | number = null;
+    //     for (let layerIndex = 0; layerIndex < layers.length; layerIndex += 1) {
+    //       if (layers[layerIndex][row][col] !== undefined && layers[layerIndex][row][col] !== null) {
+    //         if (layerIndex === 0) {
+    //           tmpColorBlend = layers[layerIndex][row][col];
+    //         } else if (tmpColorBlend !== null) {
+    //           tmpColorBlend = Number(mixColors(tmpColorBlend, layers[layerIndex][row][col]));
+    //         }
+    //         blendedPixelsRow[col] = tmpColorBlend;
+    //       }
+    //       resultOfBlend[row] = blendedPixelsRow;
+    //     }
+    //   }
+    // }
 
     return resultOfBlend;
   }
@@ -109,6 +150,37 @@ export class Renderer {
     const {
       width, height, pixelSize, realPixels, virtualPixels, ctx, imageData, lastFrameTime,
     } = this;
+
+    const newLayer: IPixelsLayer = {
+      x: 10,
+      y: 10,
+      width: 1,
+      height: 3,
+      pixels: new Uint32Array(1 * 3),
+    };
+
+    newLayer.pixels[0] = 0x22000000;
+    newLayer.pixels[1] = 0x22000000;
+    newLayer.pixels[2] = 0x22000000;
+
+    const virtualPixelsLayer: IPixelsLayer = {
+      x: 0,
+      y: 0,
+      width: width / pixelSize,
+      height: height / pixelSize,
+      pixels: virtualPixels,
+    };
+
+    const resultVirtualPixels = this.blendPixelLayers(
+      [virtualPixelsLayer, newLayer],
+      width / pixelSize,
+      height / pixelSize,
+    );
+
+    if (!resultVirtualPixels) {
+      console.error('gefdg');
+      return;
+    }
 
     performance.mark('start');
 
@@ -122,7 +194,7 @@ export class Renderer {
 
         const virtualPixelIndex = toLinearArrayIndex(col, row, widthPixelsRatio, heightPixelsRatio);
 
-        const virtualPixel = virtualPixels[virtualPixelIndex];
+        const virtualPixel = resultVirtualPixels[virtualPixelIndex];
 
         for (let realPixelX = 0; realPixelX < pixelSize; realPixelX++) {
           const realPixelXConstant = colPixelOffset + realPixelX;
